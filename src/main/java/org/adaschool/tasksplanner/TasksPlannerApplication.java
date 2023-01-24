df9367db583f8adbaa7c4b19a825b9240170946f
sancarbar <sancabar@gmail.com>
2023-01-23 22:55:32 -0500

    Initial commit with ada-client and gradle task to run tests.

---
+++ src/main/java/org/adaschool/tasksplanner/TasksPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.adaschool.tasksplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/adaschool/tasksplanner/TasksPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.tasksplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
