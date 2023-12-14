f597b47e4026cb217a877e1dacfe3453c9b508ab
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2023-12-14 13:44:53 -0500

    Proyecto Spring-boot-java-assignments con Configuracion de Swagger

--- src/main/java/org/adaschool/api/controller/product/ProductsController.java
+++ src/main/java/org/adaschool/api/controller/product/ProductsController.java
@@ -1,6 +1,9 @@
 package org.adaschool.api.controller.product;
 
+import org.adaschool.api.exception.ProductNotFoundException;
+import org.adaschool.api.exception.UserNotFoundException;
 import org.adaschool.api.repository.product.Product;
+import org.adaschool.api.repository.product.ProductDto;
 import org.adaschool.api.service.product.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/products/")
@@ -20,34 +24,60 @@ public class ProductsController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct() {
-        //TODO implement this method
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto product) {
+        Product newProduct =productsService.save(new Product(product));
         URI createdProductUri = URI.create("");
-        return ResponseEntity.created(createdProductUri).body(null);
+        return ResponseEntity.created(createdProductUri).body(product);
     }
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @GetMapping("/v1/getAll/")
+    public ResponseEntity<List<Product>> getAllProducts(ProductDto product) {
+        List<Product> products = productsService.all();
+        return ResponseEntity.ok((List<Product>) products);
+
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Product> findById(@PathVariable("id") String id) {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        Optional<Product> product = productsService.findById(id);
+
+        if (product.isPresent()) {
+            Product product1 = product.get();
+            return ResponseEntity.ok(product1);
+        } else {
+            throw new ProductNotFoundException(id);
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<Product> updateProduct() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @PutMapping("{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") String id, @RequestBody ProductDto productDto) {
+        Optional<Product> optionalUser = productsService.findById(id);
+
+        if (optionalUser.isPresent()) {
+            Product existinProduct = optionalUser.get();
+            existinProduct.setName(productDto.getName());
+            existinProduct.setDescription(productDto.getDescription());
+            existinProduct.setCategory(productDto.getCategory());
+            existinProduct.setPrice(productDto.getPrice());
+
+            productsService.save(existinProduct);
+
+            return ResponseEntity.ok().build();
+        }
+        else {
+            throw  new ProductNotFoundException(id);
+        }
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteProduct() {
-        //TODO implement this method
+    @DeleteMapping ("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id")String id) {
+        Optional<Product> existingUserOptional = productsService.findById(id);
 
-        return ResponseEntity.ok().build();
+        if (existingUserOptional.isPresent()) {
+            productsService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            throw new ProductNotFoundException(id);
+        }
     }
 }
--- src/main/java/org/adaschool/api/controller/user/UsersController.java
+++ src/main/java/org/adaschool/api/controller/user/UsersController.java
@@ -1,13 +1,18 @@
 package org.adaschool.api.controller.user;
 
+import org.adaschool.api.exception.UserNotFoundException;
 import org.adaschool.api.repository.user.User;
+import org.adaschool.api.repository.user.UserDto;
 import org.adaschool.api.service.user.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/users/")
@@ -20,33 +25,66 @@ public class UsersController {
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser() {
-        //TODO implement this method
-        URI createdUserUri = URI.create("");
-        return ResponseEntity.created(createdUserUri).body(null);
+
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+
+        User newUser = usersService.save(new User(userDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+
     }
 
+    /*   @GetMapping
+        public ResponseEntity<List<User>> getAllUsers() {
+            //TODO implement this method
+
+            return ResponseEntity.ok(null);
+        }*/
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    public ArrayList <User>getAllUsers()
+    {
+        return (ArrayList<User>) usersService.all();
     }
 
     @GetMapping("{id}")
     public ResponseEntity<User> findById(@PathVariable("id") String id) {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+        Optional<User> userOptional = usersService.findById(id);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return ResponseEntity.ok(user);
+        } else {
+            throw new UserNotFoundException(id);
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<User> updateUser() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(@PathVariable("id") String id, @RequestBody UserDto userDto) {
+        Optional<User> optionalUser = usersService.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setName(userDto.getName());
+            existingUser.setLastName(userDto.getLastName());
+
+            usersService.save(existingUser);
+
+            return ResponseEntity.ok().build();
+        }
+        else {
+            throw new UserNotFoundException(id);
+        }
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteUser() {
-        //TODO implement this method
-        return ResponseEntity.ok().build();
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable ("id") String id) {
+        Optional<User> existingUserOptional = usersService.findById(id);
+
+        if (existingUserOptional.isPresent()) {
+            usersService.deleteById(id);
+            return ResponseEntity.ok().build(); // Devuelve una respuesta HTTP 200 OK si la eliminación es exitosa
+        } else {
+            throw new UserNotFoundException(id); // Devuelve una respuesta HTTP 404 Not Found si el usuario no se encuentra
+        }
+
     }
 }
